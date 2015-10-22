01126e97a0d58aa36808bbc987432160d27ba88d
ComradeMudkipz <comrademudkipz@live.com>
2015-10-22 04:09:10 -0700

    Changed for style

--- ticTacToe.py
+++ ticTacToe.py
@@ -8,7 +8,9 @@ def printBoard(board):
     print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
     print('-+-+-')
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
 turn = 'X'
+
 for i in range(9):
     printBoard(theBoard)
     print('Turn for ' + turn + '. Move on which space?')
@@ -18,4 +20,5 @@ for i in range(9):
         turn = 'O'
     else:
         turn = 'X'
+
 printBoard(theBoard)
