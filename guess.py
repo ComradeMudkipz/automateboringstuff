5fb65e3f460f48ef9a7859fa6790bb3715ec501d
ComradeMudkipz <comrademudkipz@live.com>
2015-10-29 00:43:29 -0700

    Revised for no vulgarity

--- guess.py
+++ guess.py
@@ -4,7 +4,7 @@
 import random
 
 
-print('Hello inbred. What is your name?')
+print('Hello. What is your name?')
 name = input()
 
 print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
@@ -13,13 +13,13 @@ secretNumber = random.randint(1, 20)
 # print('DEBUG: Secret number is ' + str(secretNumber))
 
 for guessesTaken in range(1, 7):
-    print('Take a guess, bitch.')
+    print('Take a guess.')
     guess = int(input())
 
     if guess < secretNumber:
-        print('You stupid. Your guess is too low.')
+        print('Nope. Your guess is too low.')
     elif guess > secretNumber:
-        print('You are as high as a motherfucking kite.')
+        print('You are as high as a kite.')
     else:
         break # This condition is for the correct guess!
 
