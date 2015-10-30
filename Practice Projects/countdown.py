2f14c723f79b213b95824cceceb8a389317bdd3f
ComradeMudkipz <comrademudkipz@live.com>
2015-10-30 06:45:56 -0700

    Initial commit

---
+++ Practice Projects/countdown.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# countdown.py - A simple countdown script.
+
+import time
+import subprocess
+
+
+timeLeft = 60
+while timeLeft > 0:
+    print(timeLeft, end='')
+    time.sleep(1)
+    timeLeft = timeLeft - 1
+
+# At the end of the countdown, play a sound file.
+subprocess.Popen('open', 'alarm.wav')       # Change *.wav to preferred sound.
