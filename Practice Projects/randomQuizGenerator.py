b015b9069f83fcaff5e28cad77ea6fb24913b66a
ComradeMudkipz <comrademudkipz@live.com>
2015-11-29 05:32:07 -0800

    Added capitals. Fixed improper indentation.

--- Practice Projects/randomQuizGenerator.py
+++ Practice Projects/randomQuizGenerator.py
@@ -2,31 +2,12 @@
 # randomQuizGenerator.py - Creates quizzes with questions and answers in
 # random order, along with the answer key.
 
-
 import random
-
 
 # The quiz data. Keys are states and values are their capitals.
 # Add quiz data with states and capitals
-# TODO: Reformat for functionality
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
-'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
-'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
-'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
-'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
-'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
-'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston',
-'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
-'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
-'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
-'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
-'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
-'South Carolina': 'Columbia', 'South Dakota': 'Pierre',
-'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City',
-'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
-'West Virgina': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
-
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virgina': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+capitalsItems = list(capitals.items())
 
 # Generate 35 quiz files.
 for quizNum in range(35):
@@ -54,13 +35,13 @@ for quizNum in range(35):
         random.shuffle(answerOptions)
 
 
-        # TODO: Write the question and answer options to the quiz file.
+        # Write the question and answer options to the quiz file.
         quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
         for i in range(4):
-            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+            quizFile.write('    %s. %s\n' % ('ABCD'[i], answerOptions[i]))
         quizFile.write('\n')
 
         # Write the answer key to a file.
         answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
-        quizFile.close()
-        answerKeyFile.close()
+    quizFile.close()
+    answerKeyFile.close()
